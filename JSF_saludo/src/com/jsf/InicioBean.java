422489980694acb3c39f5896de148f01a31dd00d
Sergio <sergio@mail.com>
2013-03-05 12:31:44 +0000

    version 1.0

---
+++ JSF_saludo/src/com/jsf/InicioBean.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.jsf;
+
+/**
+ * @author sergio
+ *
+ */
+public class InicioBean{
+	private String nombre; // nombre del usuario
+	private Integer edad; // edad del usuario	
+
+	public String getNombre(){
+		return nombre;
+	}
+
+	public void setNombre(String nombre){
+		this.nombre=nombre;
+	}
+
+	public Integer getEdad(){
+		return edad;
+	}
+
+	public void setEdad(Integer edad){
+		this.edad=edad;
+	}
+
+}
