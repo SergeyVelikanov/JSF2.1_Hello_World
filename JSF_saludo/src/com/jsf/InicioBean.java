7e87997e10b019cf21a9b561c185f2d4303b4f73
Sergio <sergio@mail.com>
2013-07-27 17:52:20 +0100

    v.1.0

--- JSF_saludo/src/com/jsf/InicioBean.java
+++ JSF_saludo/src/com/jsf/InicioBean.java
@@ -3,10 +3,13 @@
  */
 package com.jsf;
 
+import java.io.Serializable;
+
 /**
  *
  */
-public class InicioBean{
+public class InicioBean implements Serializable{	
+	private static final long serialVersionUID = -8517600785953946612L;
 	private String nombre; // nombre del usuario
 	private Integer edad; // edad del usuario	
 
